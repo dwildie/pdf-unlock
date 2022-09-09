873c9d8ef3ebb66a6e72c43312f1c6a309706f72
dwildie <damian@wildie.com>
2022-09-10 08:40:42 +1000

    Created

---
+++ src/main/java/com/wildie/PDFUnlock.java
@@ -0,0 +1,37 @@
+package com.wildie;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ */
+public class PDFUnlock {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            usage();
+            return;
+        }
+
+        File src = new File(args[0]);
+        if (!src.exists()) {
+            System.out.printf("PDF file %s does not exist\n", src.getPath());
+            return;
+        }
+
+        File dst = new File(args[1]);
+        if (!dst.getParentFile().exists()) {
+            System.out.printf("Output directory %s does not exist\n", dst.getParentFile().getPath());
+            return;
+        }
+
+        new PdfDocument(new PdfReader(src).setUnethicalReading(true), new PdfWriter(dst)).close();
+    }
+
+    protected static void usage() {
+        System.out.println("Usage: java -jar pdf-unlock-1.0.jar <locked.pdf> <unlocked.pdf>");
+    }
+}
